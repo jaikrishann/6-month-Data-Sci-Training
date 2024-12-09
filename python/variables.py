7d5ade72e2c2bd0eaf19fad8d1e555b679752fe2
jaikrishann <jaikrishan2001@gmail.com>
2024-12-09 17:28:41 +0530

    new

---
+++ python/variables.py
@@ -0,0 +1,24 @@
+## variables rules 
+    # 
+    # Must start with a letter or underscore (_)
+    # Can contain letters, numbers, underscores (_)
+    # Must not exceed 30 characters
+    # Cannot contain any special characters or whitespace
+    # Cannot be a reserved keyword in Python
+    # Cannot be a built-in function or variable name
+    # case sensitive
+
+    #  AGE , age , Age --
+
+    #  _age, age_123, _123age -- valid
+
+
+
+    ## data types
+    # integer, float, boolean, string, list, tuple, set, dictionary, 
+
+
+   
+
+
+ 
