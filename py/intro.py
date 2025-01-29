270196195e221de2a85f453f0917af67277aec38
jaikrishann <jaikrishan2001@gmail.com>
2025-01-29 14:19:48 +0530

    new

---
+++ py/intro.py
@@ -0,0 +1,97 @@
+# print("hello world")
+
+##string 
+# s = """hello
+
+# hadsghjsd
+# saydgjhsa
+# sjdahahsj
+# hjsa
+# """
+# print(type(s))
+# print(s)
+
+##denote 
+
+# a = "" , ' ',""" """
+
+
+# s = " HelloWorld "
+# print(s.upper())
+# print(s.lower())
+# print(s.title())
+# print(s.capitalize())
+
+# print(s.replace("World", "Python"))
+# print(s.isalpha())
+# print(s.isdigit())
+# print(s.isalnum())
+# print(s.isspace())
+# print(s.startswith("W"))
+# print(s.endswith("d"))
+# print(len(s))
+
+# print(s.strip())
+# print(s.lstrip())
+# print(s.rstrip())
+# s = "helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+# print(s.zfill(7))
+# print(s.center(20, "*"))
+# print(s.ljust(20, "*"))
+# print(s.rjust(20, "*"))
+
+# print(s.count("o"))
+
+# s = "hello.py"
+# print(s.removeprefix("hello"))
+# print(s.removesuffix(".py"))
+
+
+##indexing --- numbering 
+# h e l l o 
+# 0 1 2 3 4
+# -5  -4 -3  -2 -1
+
+
+# s = "Hello World"
+# print(s[-1])
+# print(s[10])
+
+# [start:stop:step/skip]
+
+# print(s[0:7:1])
+# print(s[-1])
+##slicing 
+
+# print(s[-5:])
+# s = "01234567891"
+# print(s[::2])
+
+# s = "Hello Worldoo"
+# print(s.index("o"))
+# print(s.rindex("o"))
+# print(s.find("orl"))
+
+# s = "hello "
+# a = "world"
+# print(s +" " +a)
+# print(s.join("-"))
+
+
+# print("hello".encode('utf-8'))  
+
+# print(b'hello'.decode('utf-8')) 
+# s = "hello world"
+# print(s.join(" "))
+
+## formatted string 
+
+# age = 10 
+# text = "MY age is " + age
+# print(text)
+
+
+# age = 10 
+# text = f"MY age is {age}"
+
+# print(text)
